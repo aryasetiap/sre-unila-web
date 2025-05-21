3c89452c2801feb9921bedd77120eea055080788
aryasetiap <aryasetiap.code@gmail.com>
2025-05-21 14:04:20 +0700

    feat: Add contact information section to ContactUsEsf component

--- src/pages/esf/ContactUsEsf.jsx
+++ src/pages/esf/ContactUsEsf.jsx
@@ -12,6 +12,44 @@ const ContactUsEsf = () => {
         image='/assets/esf/contact-us/bg-hero-contactusesf.png'
         tagline="#ReGreentheFuture"
       />
+
+      <section className="py-16 bg-gray-50 text-gray-800">
+        <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row items-center gap-12">
+          {/* Bagian Kiri: Teks */}
+          <div className="md:w-1/2">
+            <h1 className="text-4xl font-bold mb-6 text-green-800">
+              GET IN TOUCH WITH ESF 2025
+            </h1>
+            <p className="mb-4 text-lg leading-relaxed text-justify">
+              We're happy to connect with participants, partners, media, or anyone interested in Environmental Sustainability Forum 2025. Contact our team through any of the channels below:
+            </p>
+            <p className="mb-4 text-lg leading-relaxed text-justify">
+              <span className="font-bold">WhatsApp</span> <br />
+              • Rahmanda Isnaina Hanum - 0856 0963 1320 <br />
+              • Aldo Syaputra - 0895 6121 78944
+            </p>
+            <p className="mb-4 text-lg leading-relaxed text-justify">
+              <span className="font-bold">Email</span> <br />
+              esforum.sreunila@gmail.com
+            </p>
+            <p className="mb-4 text-lg leading-relaxed text-justify">
+              <span className="font-bold">Instagram</span> <br />
+              @esfindonesia
+            </p>
+            <a
+              href="mailto:esforum.sreunila@gmail.com"
+              className="inline-block font-bold mt-6 px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-800 transition-colors"
+            >
+              CONTACT US
+            </a>
+
+
+
+          </div>
+        </div>
+
+      </section >
+
       <Footer />
     </>
   )
