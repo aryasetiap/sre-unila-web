6b1396bfabb1de85bd50f4b11c1b3bf191451bc7
aryasetiap <aryasetiap.code@gmail.com>
2025-05-21 21:54:38 +0700

    feat: Add links to articles in ArticlesSection for improved navigation

--- src/components/sre/articles/ArticlesSection.jsx
+++ src/components/sre/articles/ArticlesSection.jsx
@@ -6,18 +6,19 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 
 const articles = [
-    { id: 11, image: "/assets/wedpaper/wedpaper11.png" },
-    { id: 10, image: "/assets/wedpaper/wedpaper10.jpg" },
-    { id: 9, image: "/assets/wedpaper/wedpaper9.jpg" },
-    { id: 8, image: "/assets/wedpaper/wedpaper8.jpg" },
-    { id: 7, image: "/assets/wedpaper/wedpaper7.jpg" },
-    { id: 6, image: "/assets/wedpaper/wedpaper6.jpg" },
-    { id: 5, image: "/assets/wedpaper/wedpaper5.jpg" },
-    { id: 4, image: "/assets/wedpaper/wedpaper4.jpg" },
-    { id: 3, image: "/assets/wedpaper/wedpaper3.jpg" },
-    { id: 2, image: "/assets/wedpaper/wedpaper2.jpg" },
-    { id: 1, image: "/assets/wedpaper/wedpaper1.jpg" },
+    { id: 11, image: "/assets/wedpaper/wedpaper11.png", link: "https://www.instagram.com/p/DJozyIozLLv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 10, image: "/assets/wedpaper/wedpaper10.jpg", link: "https://www.instagram.com/p/DJW50UWTLJg/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 9, image: "/assets/wedpaper/wedpaper9.jpg", link: "https://www.instagram.com/p/DI2mVazz4Pm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 8, image: "/assets/wedpaper/wedpaper8.jpg", link: "https://www.instagram.com/p/DHh9ib2Je3L/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 7, image: "/assets/wedpaper/wedpaper7.jpg", link: "https://www.instagram.com/p/DHFOv9CTThE/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 6, image: "/assets/wedpaper/wedpaper6.jpg", link: "https://www.instagram.com/p/DDhmVNcTvzK/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 5, image: "/assets/wedpaper/wedpaper5.jpg", link: "https://www.instagram.com/p/DDPUzZnzyFN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 4, image: "/assets/wedpaper/wedpaper4.jpg", link: "https://www.instagram.com/p/DC4P85GStuv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 3, image: "/assets/wedpaper/wedpaper3.jpg", link: "https://www.instagram.com/p/DCmYbsbztpr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 2, image: "/assets/wedpaper/wedpaper2.jpg", link: "https://www.instagram.com/p/C_z52RkpEp2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+    { id: 1, image: "/assets/wedpaper/wedpaper1.jpg", link: "https://www.instagram.com/p/C-7iADWTH48/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
 ];
+
 
 const ArticlesSection = () => {
     return (
@@ -50,46 +51,16 @@ const ArticlesSection = () => {
                 <div className="hidden sm:block">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {articles.map((article) => (
-                            <motion.div
-                                initial={{ opacity: 0, y: 30 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                transition={{ duration: 0.6, delay: article.id * 0.1 }}
-                                key={article.id}
-                                className="px-4 bg-[#F9F6EE] rounded-3xl"
+                            <a
+                                href={article.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
                             >
-                                <div className="flex items-center justify-start rounded-lg pt-4 pb-2 gap-4">
-                                    <img src="/assets/wedpaper/logo.png" alt="logo-sre" />
-                                    <p className="font-semibold text-black">sre.unila</p>
-                                </div>
-                                <img
-                                    src={article.image}
-                                    alt={`WEDPAPER #${article.id}`}
-                                    className="w-full rounded-lg shadow-lg"
-                                />
-                                <div className="flex flex-col py-4 gap-2">
-                                    <div className="flex gap-2 items-center justify-start">
-                                        <img src="/assets/wedpaper/heart.png" alt="" />
-                                        <img src="/assets/wedpaper/comment.png" alt="" />
-                                        <img src="/assets/wedpaper/plane.png" alt="" />
-                                    </div>
-                                    <p className="text-black">
-                                        <span className="font-bold">sre.unila</span> [WEDPAPER #{article.id}]
-                                    </p>
-                                </div>
-                            </motion.div>
-                        ))}
-                    </div>
-                </div>
-
-                {/* Carousel hanya untuk layar kecil */}
-                <div className="block sm:hidden">
-                    <Swiper modules={[Pagination]} spaceBetween={16} slidesPerView={1.2} pagination={{ clickable: true }}>
-                        {articles.map((article) => (
-                            <SwiperSlide key={article.id}>
                                 <motion.div
                                     initial={{ opacity: 0, y: 30 }}
                                     animate={{ opacity: 1, y: 0 }}
                                     transition={{ duration: 0.6, delay: article.id * 0.1 }}
+                                    key={article.id}
                                     className="px-4 bg-[#F9F6EE] rounded-3xl"
                                 >
                                     <div className="flex items-center justify-start rounded-lg pt-4 pb-2 gap-4">
@@ -112,6 +83,48 @@ const ArticlesSection = () => {
                                         </p>
                                     </div>
                                 </motion.div>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+
+                {/* Carousel hanya untuk layar kecil */}
+                <div className="block sm:hidden">
+                    <Swiper modules={[Pagination]} spaceBetween={16} slidesPerView={1.2} pagination={{ clickable: true }}>
+                        {articles.map((article) => (
+                            <SwiperSlide key={article.id}>
+                                <a
+                                    href={article.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 30 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.6, delay: article.id * 0.1 }}
+                                        className="px-4 bg-[#F9F6EE] rounded-3xl"
+                                    >
+                                        <div className="flex items-center justify-start rounded-lg pt-4 pb-2 gap-4">
+                                            <img src="/assets/wedpaper/logo.png" alt="logo-sre" />
+                                            <p className="font-semibold text-black">sre.unila</p>
+                                        </div>
+                                        <img
+                                            src={article.image}
+                                            alt={`WEDPAPER #${article.id}`}
+                                            className="w-full rounded-lg shadow-lg"
+                                        />
+                                        <div className="flex flex-col py-4 gap-2">
+                                            <div className="flex gap-2 items-center justify-start">
+                                                <img src="/assets/wedpaper/heart.png" alt="" />
+                                                <img src="/assets/wedpaper/comment.png" alt="" />
+                                                <img src="/assets/wedpaper/plane.png" alt="" />
+                                            </div>
+                                            <p className="text-black">
+                                                <span className="font-bold">sre.unila</span> [WEDPAPER #{article.id}]
+                                            </p>
+                                        </div>
+                                    </motion.div>
+                                </a>
                             </SwiperSlide>
                         ))}
                     </Swiper>
