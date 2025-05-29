32483e38e703be754eac6e4f399e668f02b5662c
aryasetiap <aryasetiap.code@gmail.com>
2025-05-30 00:45:32 +0700

    feat: Refactor AgendaESFSection layout for improved structure and responsiveness

--- src/components/esf/home/AgendaESFSection.jsx
+++ src/components/esf/home/AgendaESFSection.jsx
@@ -16,78 +16,82 @@ const AgendaSection = () => {
                     ESF AGENDA 2025
                 </h2>
                 <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-8">
-                    <motion.div
-                        className="bg-white shadow-lg rounded-xl pb-6 overflow-hidden"
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.7 }}
-                        viewport={{ once: true }}
-                    >
-                        <img
-                            src="/assets/esf/home/plenary.png"
-                            alt="Plenary"
-                            className="w-full h-auto"
-                        />
-                        <h3 className="text-left text-2xl text-[#1D6935] px-6 font-bold mt-2 pt-2">
-                            Plenary Meeting
-                        </h3>
-                        <p className="text-left text-base text-[#1D6935] mt-4 px-6">
-                            A key agenda featuring six panel discussions on SDGs 7, 8, 9, 11, 12,
-                            and 13, fostering engagement between speakers and participants. This
-                            session aims to build synergy among experts, policymakers, academics,
-                            entrepreneurs, students, and communities at the domestic, national, and
-                            international levels to advance SDGs at the subnational scale.
-                        </p>
-                        <Link to="/esf/agenda">
-                            <button className="mt-4 bg-transparent border-[#1D6935] border-2 text-[#1D6935] py-2 px-4 rounded-2xl hover:bg-[#1D6935] hover:text-white transition duration-300">
-                                SEE MORE
-                            </button>
-                        </Link>
-                        <p className="mt-4 text-[#1D6935] font-bold">
+                    <div>
+                        <motion.div
+                            className="bg-white shadow-lg rounded-xl pb-6 overflow-hidden"
+                            initial={{ opacity: 0, y: 50 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.7 }}
+                            viewport={{ once: true }}
+                        >
+                            <img
+                                src="/assets/esf/home/plenary.png"
+                                alt="Plenary"
+                                className="w-full h-auto"
+                            />
+                            <h3 className="text-left text-2xl text-[#1D6935] px-6 font-bold mt-2 pt-2">
+                                Plenary Meeting
+                            </h3>
+                            <p className="text-left text-base text-[#1D6935] mt-4 px-6">
+                                A key agenda featuring six panel discussions on SDGs 7, 8, 9, 11, 12,
+                                and 13, fostering engagement between speakers and participants. This
+                                session aims to build synergy among experts, policymakers, academics,
+                                entrepreneurs, students, and communities at the domestic, national, and
+                                international levels to advance SDGs at the subnational scale.
+                            </p>
+                            <Link to="/esf/agenda">
+                                <button className="mt-4 bg-transparent border-[#1D6935] border-2 text-[#1D6935] py-2 px-4 rounded-2xl hover:bg-[#1D6935] hover:text-white transition duration-300">
+                                    SEE MORE
+                                </button>
+                            </Link>
+                        </motion.div>
+                        <p className="lg:text-left mt-4 text-[#1D6935] font-bold">
                             DAY 1 Environmental Sustainability Forum 2025
                         </p>
-                    </motion.div>
+                    </div>
 
-                    <motion.div
-                        className="bg-white shadow-lg rounded-xl pb-6 overflow-hidden"
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.7, delay: 0.2 }}
-                        viewport={{ once: true }}
-                    >
-                        <img
-                            src="/assets/esf/home/thematic.png"
-                            alt="Thematic"
-                            className="w-full h-auto"
-                        />
-                        <h3 className="text-left text-2xl text-[#1D6935] px-6 font-bold mt-2 pt-2">
-                            Thematic Meeting
-                        </h3>
-                        <div className="text-left text-base text-[#1D6935] mt-4 px-6">
-                            An exclusive session focused on specific SDG topics, particularly
-                            addressing opportunities and challenges in sustainable development
-                            within Lampung through cross-sector collaboration. Expected outcomes
-                            include:
-                            <ul className="list-disc list-inside mt-2">
-                                <li>
-                                    A resolution and roadmap as concrete solutions for sustainable
-                                    development in Lampung.
-                                </li>
-                                <li>
-                                    Presentation of findings to Lampung policymakers to support policy
-                                    formulation.
-                                </li>
-                            </ul>
-                        </div>
-                        <Link to="/esf/agenda">
-                            <button className="mt-4 bg-transparent border-[#1D6935] border-2 text-[#1D6935] py-2 px-4 rounded-2xl hover:bg-[#1D6935] hover:text-white transition duration-300">
-                                SEE MORE
-                            </button>
-                        </Link>
-                        <p className="mt-4 text-[#1D6935] font-bold">
+                    <div>
+                        <motion.div
+                            className="bg-white shadow-lg rounded-xl pb-6 overflow-hidden"
+                            initial={{ opacity: 0, y: 50 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.7, delay: 0.2 }}
+                            viewport={{ once: true }}
+                        >
+                            <img
+                                src="/assets/esf/home/thematic.png"
+                                alt="Thematic"
+                                className="w-full h-auto"
+                            />
+                            <h3 className="text-left text-2xl text-[#1D6935] px-6 font-bold mt-2 pt-2">
+                                Thematic Meeting
+                            </h3>
+                            <div className="text-left text-base text-[#1D6935] mt-4 px-6">
+                                An exclusive session focused on specific SDG topics, particularly
+                                addressing opportunities and challenges in sustainable development
+                                within Lampung through cross-sector collaboration. Expected outcomes
+                                include:
+                                <ul className="list-disc list-inside mt-2">
+                                    <li>
+                                        A resolution and roadmap as concrete solutions for sustainable
+                                        development in Lampung.
+                                    </li>
+                                    <li>
+                                        Presentation of findings to Lampung policymakers to support policy
+                                        formulation.
+                                    </li>
+                                </ul>
+                            </div>
+                            <Link to="/esf/agenda">
+                                <button className="mt-4 bg-transparent border-[#1D6935] border-2 text-[#1D6935] py-2 px-4 rounded-2xl hover:bg-[#1D6935] hover:text-white transition duration-300">
+                                    SEE MORE
+                                </button>
+                            </Link>
+                        </motion.div>
+                        <p className="lg:text-left mt-4 text-[#1D6935] font-bold">
                             DAY 2 Environmental Sustainability Forum 2025
                         </p>
-                    </motion.div>
+                    </div>
                 </div>
             </motion.div>
         </section>
